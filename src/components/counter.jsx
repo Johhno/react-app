1460cbbc4ffa1a20b824a54a03038f4291c97817
John Tchen <johntchen.mos@gmail.com>
2023-10-24 23:13:25 +0200

    Update State

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -12,7 +12,8 @@ class Counter extends Component {
     // }
 
     handleIncrement = () => {
-        console.log('Increment Clicked', this);
+        this.state.count++;
+        this.setState({ count: this.state.count + 1 });
     }
     
     getBadgeClasses() {
