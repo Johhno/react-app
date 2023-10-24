352e5587be9987ebe19165386bf412854bb50035
John Tchen <johntchen.mos@gmail.com>
2023-10-24 14:42:19 +0200

    Named and Default Export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-import { Teacher } from './Teacher.js'
+import Teacher, { promote } from './Teacher.js';
+import React, { Component } from 'react';
+
+
+// Default -> import ... from '';
+// Named -> import { ... } from '';
 
 const teacher = new Teacher("John", 'BSc');
 teacher.teach();
