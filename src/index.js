8a24960ee3a3e67c0913911c445566618c3bf546
John Tchen <johntchen.mos@gmail.com>
2023-10-24 02:59:53 +0200

    template literal

--- src/index.js
+++ src/index.js
@@ -1,28 +1,3 @@
-// can be called
-// var => function
-// const - let => block
-
-const person = {
-    talk() {
-
-        var self = this;
-        setTimeout(function () {
-            console.log("self", self);
-        }, 1000);
-
-    }
-};
-
-const personBis = {
-    talk() {
-
-        var self = this;
-        setTimeout(() => {
-            console.log("this", this);
-        }, 1);
-
-    }
-};
-
-person.talk();
-
+const colors = ['red', 'green', 'blue'];
+const items = colors.map(color => `<li> ${color}  </li>`); // template literal
+console.log(items);
