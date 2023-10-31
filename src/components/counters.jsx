0ee3cc34098f16e257ad6c5c36d4029c55ebb9c3
John Tchen <johntchen.mos@gmail.com>
2023-10-31 13:12:21 +0100

    Reset button & function

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -17,9 +17,18 @@ class Counters extends Component {
         console.log('Event Handler Called', counterId);
     };
 
+    handleReset = () => {
+        const counters = this.state.counters.map(c => {
+            c.value = 0;
+            return c;
+        });
+        this.setState({ counters });
+    };
+
     render() {
         return (
             <div>
+                <button onClick={this.handleReset} className="btn btn-primary btn-sm m-2">Reset</button>
                 {
                     this.state.counters.map(counter => (
                         <Counter
